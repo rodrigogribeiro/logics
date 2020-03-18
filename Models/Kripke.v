da48f8ec3ef64e8aadc5d74c26c4ae269c144411
Rodrigo <rodrigogribeiro@gmail.com>
2020-03-18 17:07:40 -0300

    Almost done. Need to prove soundness.

--- Models/Kripke.v
+++ Models/Kripke.v
@@ -7,40 +7,57 @@ Require Import
 
 (** definition of the Kripke model *)
 
-Reserved Notation "w '||-'" (at level 40, no associativity).
-Reserved Infix "<<"  (at level 40, no associativity).
+Section KRIPKE.
 
-Class kripke (world : Type) : Type
-  := {
-      lt_world : world -> world -> Type 
-      where "w1 '<<' w2" := (lt_world w1 w2) ;
+  Reserved Notation "w '||-'" (at level 40, no associativity).
+  Reserved Infix "<<"  (at level 40, no associativity).
 
-      lt_world_refl : forall w, w << w ;
-      lt_world_trans : forall w1 w2 w3, w1 << w2 ->
-                                   w2 << w3 ->
-                                   w1 << w3 ;
-      forces : world -> Type
-      where "w '||-'" := (forces w) ;
+  Parameter world : Type.
 
-       forces_lt_mono : forall w w',
-           w << w' -> w ||- -> w' ||- 
-     }.
+  Parameter lt_world : world -> world -> Type.
+  Notation  "w1 '<<' w2" := (lt_world w1 w2).
 
-Declare Scope kripke_scope.
-Bind Scope kripke_scope with kripke.
-Delimit Scope kripke_scope with kripke. 
+  Parameter lt_world_refl : forall w, w << w.
+  Parameter lt_world_trans : forall w1 w2 w3,
+      w1 << w2 ->
+      w2 << w3 ->
+      w1 << w3.
 
-Notation "w << w1" := (lt_world w w1)(at level 40, no associativity).
-Notation "w ||-"   := (forces w)(at level 40, no associativity).
+  Parameter forces : world -> Type.
+  Notation "w '||-'" := (forces w).
 
-Equations forces_ty {W}`{kripke W}(w : W)(t : ty) : Type :=
-  forces_ty w Base        := forces w ;
-  forces_ty w (t1 ==> t2) := forall w', w << w' -> forces_ty w' t1 -> forces_ty w' t2.
+  Parameter forces_lt_mono
+    : forall w w', w << w' -> w ||- -> w' ||-.
 
-Equations forces_con {W}`{kripke W}(w : W)(G : con) : Type :=
-  forces_con w []       := unit ;
-  forces_con w (t :: G)  := forces_con w G * forces_ty w t.
 
-Definition forcing {W}`{kripke W} G t :=
-  forall (w : W), forces_con w G -> forces_ty w t.
+  Equations forces_ty (w : world)(t : ty) : Type :=
+    forces_ty w Base        := forces w ;
+    forces_ty w (t1 ==> t2) := forall w', w << w' -> forces_ty w' t1 -> forces_ty w' t2.
 
+  Equations forces_con (w : world)(G : con) : Type :=
+    forces_con w []       := unit ;
+    forces_con w (t :: G)  := forces_con w G * forces_ty w t.
+
+  Definition forcing G t :=
+    forall (w : world), forces_con w G -> forces_ty w t.
+
+  Equations ty_mono {w w'} t (p : w << w')
+    : forces_ty w t -> forces_ty w' t :=
+    ty_mono Base        p d := forces_lt_mono _ _ p d ;
+    ty_mono (t1 ==> t2) p d := fun _ q r => d _ (lt_world_trans _ _ _ p q) r.
+
+  Equations con_mono G {w w'}
+    : w << w' -> forces_con w G -> forces_con w' G :=
+    con_mono []       p q := q ;
+    con_mono (t :: G') p q := (con_mono G' p (fst q), ty_mono t p (snd q)).
+
+  Equations member_mono {G t}(v : member t G) : forcing G t :=
+    member_mono Here      w (_, q) := q ;
+    member_mono (There v) w (p, q) := member_mono v w p.
+
+  Equations term_mono {G t}(e : term G t) : forcing G t :=
+    term_mono (Var v)         w p   := member_mono v w p;
+    term_mono (Abs e)         w p   := fun _ q r => term_mono e _ (con_mono _ q p, r) ;
+    term_mono (App e e')      w p
+       := term_mono e w p _ (lt_world_refl w) (term_mono e' w p).
+End KRIPKE.
